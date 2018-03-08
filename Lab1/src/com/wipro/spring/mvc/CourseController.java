d6f4d0a9044beb0292b70f7a6f0ab978d76d7558
AMIT KUMAR YADAV <AMIT KUMAR YADAV@Amit>
2018-03-08 15:26:12 +0530

    3

--- Lab1/src/com/wipro/spring/mvc/CourseController.java
+++ Lab1/src/com/wipro/spring/mvc/CourseController.java
@@ -9,10 +9,13 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
 public class CourseController extends AbstractController{
+
 	@Autowired
 private CourseService cs;
 protected ModelAndView handleRequestInternal(HttpServletRequest request,HttpServletResponse response) throws Exception {
 	ArrayList<String> al=cs.getCourses();
+	System.out.println("...............");
 	return new ModelAndView("show","AL",al);
+	
 }
 }
